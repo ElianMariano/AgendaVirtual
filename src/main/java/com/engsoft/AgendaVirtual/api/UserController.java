3d7f128d13858b43228245c26bd16d0949368c45
Elian Mariano <elianmarianogabriel@gmail.com>
2021-07-07 15:50:56 -0300

    Alterar usuários

--- src/main/java/com/engsoft/AgendaVirtual/api/UserController.java
+++ src/main/java/com/engsoft/AgendaVirtual/api/UserController.java
@@ -2,6 +2,7 @@ package com.engsoft.AgendaVirtual.api;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,10 +30,31 @@ public class UserController {
     public Iterable <User> listUsers(){
     	return repository.findAll();
     }
+
+    // Altera os usuarios
+    @PutMapping("/user/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return repository.findById(id)
+            .map(user -> {
+                user.setNome(newUser.getNome());
+                user.setTelefone(newUser.getTelefone());
+                user.setCidade(newUser.getCidade());
+                user.setRua(newUser.getRua());
+                user.setUf(newUser.getUf());
+                user.setSenha(newUser.getSenha());
+                user.setEmail(newUser.getEmail());
+                user.setNascimento(newUser.getNascimento());
+                return repository.save(user);
+            })
+            .orElseGet(() -> {
+                newUser.setId(id);
+                return repository.save(newUser);
+            });
+    }
     
     //Remover usuários
-    @DeleteMapping(path = "/user/{id}")
-    public void deleteUser(@PathVariable (name = "id", required = true) Long id){
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable Long id){
     	repository.deleteById(id);
     }
 }
