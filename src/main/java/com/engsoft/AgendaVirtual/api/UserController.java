104ba0a599cee16a1b110505cddd7a268248a8bc
RaphaelMacedoB <faeldojo@gmail.com>
2021-07-07 13:57:05 -0300

    Remover Usuário

--- src/main/java/com/engsoft/AgendaVirtual/api/UserController.java
+++ src/main/java/com/engsoft/AgendaVirtual/api/UserController.java
@@ -1,5 +1,8 @@
 package com.engsoft.AgendaVirtual.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +30,9 @@ public class UserController {
     	return repository.findAll();
     }
     
+    //Remover usuários
+    @DeleteMapping(path = "/user/{id}")
+    public void deleteUser(@PathVariable (name = "id", required = true) Long id){
+    	repository.deleteById(id);
+    }
 }
