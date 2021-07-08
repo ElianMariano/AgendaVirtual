e2ed27f1ddd5280e558b1495a4ef58628a67e026
Elian Mariano <elianmarianogabriel@gmail.com>
2021-07-08 09:59:10 -0300

    Alterar tarefa

--- src/main/java/com/engsoft/AgendaVirtual/api/TasksController.java
+++ src/main/java/com/engsoft/AgendaVirtual/api/TasksController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,12 +31,26 @@ public class TasksController {
     public Iterable <Tasks> listTasks(){
     	return repository.findAll();
     }
+
+    // Alterar tarefas
+    @PutMapping("/task/{id}")
+    public Tasks updateTask(@RequestBody Tasks newTask, @PathVariable Long id){
+        return repository.findById(id)
+            .map(task -> {
+                task.setDia(newTask.getDia());
+                task.setTitulo(newTask.getTitulo());
+                task.setFeito(newTask.getFeito());
+                return repository.save(task);
+            })
+            .orElseGet(() -> {
+                newTask.setId(id);
+                return repository.save(newTask);
+            });
+    }
     
     //Remover tarefas
     @DeleteMapping("/task/{id}")
     public void deleteTasks(@PathVariable Long id){
     	repository.deleteById(id);
     }
-   
-   
 }
