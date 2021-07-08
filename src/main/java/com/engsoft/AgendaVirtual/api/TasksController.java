546fad1ba3868b042c96b8cb563381b5b8e1bc3a
RaphaelMacedoB <faeldojo@gmail.com>
2021-07-07 21:14:21 -0300

    Remover tarefas

--- src/main/java/com/engsoft/AgendaVirtual/api/TasksController.java
+++ src/main/java/com/engsoft/AgendaVirtual/api/TasksController.java
@@ -4,7 +4,9 @@ package com.engsoft.AgendaVirtual.api;
 import com.engsoft.AgendaVirtual.model.Tasks;
 import com.engsoft.AgendaVirtual.service.TaskRepository;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,11 @@ public class TasksController {
     	return repository.findAll();
     }
     
+    //Remover tarefas
+    @DeleteMapping("/task/{id}")
+    public void deleteTasks(@PathVariable Long id){
+    	repository.deleteById(id);
+    }
    
    
 }
